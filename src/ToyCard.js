101b0e8f3bbc9352188bc4fb6354cb583e18f921
Shilcof <robert.shilcof@gmail.com>
2021-03-04 20:01:04 +0000

    updated with hooks

--- src/ToyCard.js
+++ src/ToyCard.js
@@ -1,13 +1,33 @@
-
+import {useState, useEffect} from 'react'
 
 function ToyCard(props){
     // const {id, image, name} = props.toy // destructuring is an option
+    const [timer, setTimer] = useState(0)
+    const [colour, setColour] = useState("yellow")
+
+    useEffect(()=>{
+        const interval = setInterval(() => {
+            setTimer((timer)=>timer+1)
+        }, 1000);
+
+        return () => {
+            clearInterval(interval)
+        }
+    }, [])
+
+    useEffect(()=>{
+        setColour(prevColour=>prevColour==="yellow"?"white":"yellow")
+    }, [props.likes])
+
     return(
-        <div class="card" id={`toy-${props.id}`}>
+        <div className="card" style={{backgroundColor: colour}} id={`toy-${props.id}`}>
             <h2>{props.name}</h2>
-            <img src={props.image} class="toy-avatar"/>
+            <img src={props.image} className="toy-avatar"/>
             <p>{props.likes} Likes </p>
-            <button class="like-btn">Like &lt;3</button>
+            <button className="like-btn"
+                onClick={()=>props.likeToy(props.id)}
+            >Like &lt;3</button>
+            <div>{timer}</div>
         </div>
     )
 }
--- src/ToysContainer.js
+++ src/ToysContainer.js
@@ -1,67 +1,71 @@
 import toysObj from './database'
 import ToyCard from './ToyCard'
-import React from 'react'
-
-// when state changes, a rerender is caused 
-// if we want to utilize state we need a Class Component 
-
-// const toys = 
+import React, {useState, useEffect} from 'react'
    
-class ToysContainer extends React.Component{
-    // sets INITIAL STATE
-    state = {
-        toys: [],
-        search: "",
-        whatever: "hello"
-    }
+function ToysContainer(props) {
 
-    makeToyCards(){
-        //utilize STATE
-        let displayedToys = this.state.toys
-        console.log(this.state.search)
-        if(this.state.search){
-            displayedToys = this.state.toys.filter((toy) =>  
-            toy.name.toLowerCase().includes(this.state.search.toLowerCase()))
-        }
+    const [toys, setToys] = useState([])
+    const [search, setSearch] = useState("")
 
-        return displayedToys.map(toy => <ToyCard toy={toy} id={toy.id} name={toy.name} image={toy.image} likes={toy.likes} />)
-    }
-
-    // componentDidUpdate(){
-    //     console.log("updatesd", this.state)
+    // const likeToy = (id) => {
+    //     const index = toys.findIndex(i=>i.id===id)
+    //     const updatedToy = {...toys[index], likes: toys[index].likes + 1}
+    //     setToys([...toys.slice(0, index), updatedToy,...toys.slice(index+1)])
     // }
 
-    componentDidMount(){
-      // where you make your fetch requests 
-      const url ="http://localhost:3000/toys"
-      fetch(url)
-      .then(res => res.json())
-      .then(json => {
-          // deal with the json
-          console.log(json)
-         // this.state.toys = json  //THIS WILL NOT CAUSE A RERENDER
-         this.setState({
-             toys: json
-         })
-      })
+    const likeToy = (id) => {
+        const index = toys.findIndex(i=>i.id===id)
+        const toy = toys[index]
+
+        const configObj = {
+            method: 'PATCH',
+            headers: {
+                "Content-Type": "application/json",
+                "Accepts": "application/json"
+            },
+            body: JSON.stringify({likes: toy.likes + 1})
+        }
+
+        fetch(`http://localhost:3000/toys/${id}`, configObj)
+        .then(res => res.json())
+        .then(json => {
+            setToys([...toys.slice(0, index), json,...toys.slice(index+1)])
+        })
+
     }
 
-    handleInputChange = (e) => {
+    const makeToyCards = () => {
+        let displayedToys = toys
+        if(search){
+            displayedToys = toys.filter((toy) =>  
+            toy.name.toLowerCase().includes(search.toLowerCase()))
+        }
+
+        return displayedToys.map(toy => <ToyCard key={toy.id} toy={toy} id={toy.id} name={toy.name} image={toy.image} likes={toy.likes} likeToy={likeToy}/>)
+    }
+
+    useEffect(()=>{
+        const url ="http://localhost:3000/toys"
+        fetch(url)
+        .then(res => res.json())
+        .then(json => {
+           setToys(json)
+        })
+    }, [])
+
+    const handleInputChange = (e) => {
         const search = e.target.value
-        this.setState({search: search}) // will cause a rerender
+        setSearch(search)
     }
 
-
-    render(){
-        return(
-            <div id="toy-container">
-                <div>
-                    <input type="text" placeholder="Search for a toy..." onChange={this.handleInputChange}/>
-                </div>
-               {this.makeToyCards()}
+    return(
+        <div id="toy-container">
+            <div>
+                <input type="text" placeholder="Search for a toy..." onChange={handleInputChange}/>
             </div>
-        ) 
-    }
+            {makeToyCards()}
+        </div>
+    ) 
 }
 
 export default ToysContainer
